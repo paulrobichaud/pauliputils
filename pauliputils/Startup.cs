516da391cf26905f9793a4125a5eee0f645b564b
Paul Robichaud <paul@paulrobichaud.net>
2015-08-01 22:54:39 -0500

    Initial project creation.

---
+++ pauliputils/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(pauliputils.Startup))]
+namespace pauliputils
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
